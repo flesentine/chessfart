3a837a3d4b6b62a916a974ffe2566e9dbda7772e
flesentine <chrisgmcc@gmail.com>
2026-08-31 16:39:26 -0700

    Art pass: add VGA sprite asset API

---
+++ include/ui_assets.h
@@ -0,0 +1,14 @@
+#ifndef CF_UI_ASSETS_H
+#define CF_UI_ASSETS_H
+
+#include "board.h"
+
+#define CF_UI_PIECE_W 18
+#define CF_UI_PIECE_H 16
+#define CF_UI_PUFF_W 16
+#define CF_UI_PUFF_H 16
+
+void ui_assets_draw_piece(int x, int y, const CfPiece *piece, cf_u8 background);
+void ui_assets_draw_puff(int x, int y, int frame);
+
+#endif
