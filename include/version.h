2ea6e20dfd84d57565e449ae66be3fe2c57f85a6
flesentine <chrisgmcc@gmail.com>
2026-08-31 10:40:04 -0700

    Build 12: add version header

---
+++ include/version.h
@@ -0,0 +1,12 @@
+#ifndef CF_VERSION_H
+#define CF_VERSION_H
+
+#define CF_VERSION_MAJOR 1
+#define CF_VERSION_MINOR 0
+#define CF_VERSION_PATCH 0
+#define CF_VERSION_STAGE "rc1"
+#define CF_VERSION_STRING "1.0.0-rc1"
+#define CF_RELEASE_LABEL "1.0 RC1"
+#define CF_RELEASE_ZIP "chessfart-1.0.0-rc1-dos.zip"
+
+#endif
