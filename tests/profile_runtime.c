54c79420dea1e7e0ee850f9c4973b3854c4aeab9
flesentine <chrisgmcc@gmail.com>
2026-09-22 16:49:56 -0700

    Add timeout stress and final weighted profile

--- tests/profile_runtime.c
+++ tests/profile_runtime.c
@@ -1430,6 +1430,161 @@ static void run_status_fart_profile(void)
            status_fart_profile_sink);
 }
 
+static volatile int action_gen_total_profile_sink;
+
+static void run_action_generation_total_profile(void)
+{
+    static CfCpuActionList list;
+    CfBoard board;
+    CfGasState gas;
+    clock_t start;
+    clock_t end;
+    unsigned long start_ms;
+    unsigned long gas_ms;
+    int repeat;
+    int file;
+    int start_count;
+    int gas_count;
+
+    board_init_starting_position(&board);
+    gas_init(&gas);
+    start = clock();
+    for (repeat = 0; repeat < 100000; ++repeat) {
+        cpu_generate_actions(&board, &gas, &list);
+        action_gen_total_profile_sink += list.count;
+    }
+    end = clock();
+    start_ms =
+        (unsigned long)(((end - start) * 1000L) / CLOCKS_PER_SEC);
+    cpu_generate_actions(&board, &gas, &list);
+    start_count = list.count;
+
+    for (file = 0; file < 8; ++file) {
+        gas_set(&gas, file, 1, 3U);
+        gas_set(&gas, file, 6, 3U);
+    }
+    start = clock();
+    for (repeat = 0; repeat < 100000; ++repeat) {
+        cpu_generate_actions(&board, &gas, &list);
+        action_gen_total_profile_sink += list.count;
+    }
+    end = clock();
+    gas_ms =
+        (unsigned long)(((end - start) * 1000L) / CLOCKS_PER_SEC);
+    cpu_generate_actions(&board, &gas, &list);
+    gas_count = list.count;
+
+    printf("ACTION_GEN_TOTAL start_ms=%lu start_actions=%d "
+           "gas_ms=%lu gas_actions=%d sink=%d\n",
+           start_ms, start_count, gas_ms, gas_count,
+           action_gen_total_profile_sink);
+}
+
+#ifndef CF_PROFILE_RUNTIME
+static void run_timeout_stress_case(const char *name,
+                                    CfCpuDifficulty difficulty)
+{
+    CfBoard board;
+    CfGasState gas;
+    CfGasHistory history;
+    CfCpuConfig config;
+    CfCpuAction action;
+    CfCpuStats stats;
+    clock_t start;
+    clock_t end;
+    unsigned long elapsed_ms;
+    int file;
+
+    board_init_starting_position(&board);
+    gas_init(&gas);
+    for (file = 0; file < 8; ++file) {
+        gas_set(&gas, file, 1, 3U);
+        gas_set(&gas, file, 6, 3U);
+    }
+    board.side_to_move = CF_COLOR_BLACK;
+    gas_history_init(&history, &board, &gas);
+    cpu_config_for_difficulty(&config, difficulty);
+    config.max_depth = 5;
+    config.node_budget = 0UL;
+
+    start = clock();
+    if (!cpu_choose_action(
+            &board, &gas, &history, &config, &action, &stats)) {
+        printf("TIME_STRESS %s ERROR no-action\n", name);
+        return;
+    }
+    end = clock();
+    elapsed_ms =
+        (unsigned long)(((end - start) * 1000L) / CLOCKS_PER_SEC);
+    printf("TIME_STRESS %s limit_ms=%lu elapsed_ms=%lu "
+           "nodes=%lu depth=%d cutoffs=%lu score=%d budget=%d type=%d\n",
+           name, config.time_limit_ms, elapsed_ms,
+           stats.nodes, stats.depth_completed, stats.cutoffs,
+           stats.score, stats.budget_hit, (int)action.type);
+}
+
+static void run_timeout_stress_profile(void)
+{
+    run_timeout_stress_case("EASY_100", CF_CPU_EASY);
+    run_timeout_stress_case("MED_500", CF_CPU_MEDIUM);
+    run_timeout_stress_case("HARD_1500", CF_CPU_HARD);
+}
+#endif
+
+#ifdef CF_PROFILE_RUNTIME
+static void print_search_counters(const char *name)
+{
+    const CfCpuProfileCounters *c;
+    c = cpu_internal_profile_counters();
+    printf("SEARCH_COUNTS %s full_budget=%lu recursive_budget=%lu "
+           "clock_polls=%lu node_guard=%lu action_gen=%lu sort=%lu "
+           "legal_probe=%lu eval=%lu move_attempt=%lu fart_attempt=%lu "
+           "fart_bonus=%lu\n",
+           name,
+           c->full_budget_checks, c->recursive_budget_checks,
+           c->clock_polls, c->node_guard_checks,
+           c->action_gen_calls, c->sort_calls,
+           c->legal_probe_calls, c->eval_calls,
+           c->move_attempts, c->fart_attempts,
+           c->fart_bonus_calls);
+}
+
+static void run_weighted_search_case(const char *name,
+                                     CfCpuDifficulty difficulty,
+                                     int gas_heavy)
+{
+    CfBoard board;
+    CfGasState gas;
+    CfGasHistory history;
+    CfCpuConfig config;
+    CfCpuAction action;
+    CfCpuStats stats;
+    int file;
+
+    board_init_starting_position(&board);
+    gas_init(&gas);
+    if (gas_heavy) {
+        for (file = 0; file < 8; ++file) {
+            gas_set(&gas, file, 1, 3U);
+            gas_set(&gas, file, 6, 3U);
+        }
+        board.side_to_move = CF_COLOR_BLACK;
+    }
+    gas_history_init(&history, &board, &gas);
+    cpu_config_for_difficulty(&config, difficulty);
+    cpu_internal_profile_reset();
+    if (cpu_choose_action(
+            &board, &gas, &history, &config, &action, &stats))
+        print_search_counters(name);
+}
+
+static void run_weighted_search_profile(void)
+{
+    run_weighted_search_case("HARD_START_TIMED", CF_CPU_HARD, 0);
+    run_weighted_search_case("FART_HEAVY_MED_TIMED", CF_CPU_MEDIUM, 1);
+}
+#endif
+
 static unsigned long profile_perft(CfBoard *board, int depth)
 {
     CfMoveList list;
@@ -1605,6 +1760,12 @@ int main(void)
     run_movegen_scratch_profile();
     run_action_bonus_king_profile();
     run_status_fart_profile();
+    run_action_generation_total_profile();
+#ifndef CF_PROFILE_RUNTIME
+    run_timeout_stress_profile();
+#else
+    run_weighted_search_profile();
+#endif
     run_perft_profile();
     run_profile("EASY_START", CF_CPU_EASY);
     run_profile("MED_START", CF_CPU_MEDIUM);
